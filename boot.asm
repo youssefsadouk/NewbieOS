b1978ca4ad7ee145d00dd77b00d12c3fd6cbe71c
Youssef <contact.sadouk@gmail.com>
2023-06-13 20:33:25 +0100

    modified boot.asm

--- boot.asm
+++ boot.asm
@@ -2,11 +2,27 @@ ORG 0X7C00
 BITS 16
 
 start:
-    mov ah, 0eh  ; 
-    mov al, 'A'  ;
-    mov bx, 0 
-    int 0x10      ; 
+    mov si, message
+    call print
     jmp $
-    
+
+print:  
+    mov bx, 0 
+.loop:
+    lodsb
+    cmp al, 0
+    je .end
+    call printChar
+    jmp .loop
+
+.end: ret
+
+printChar: 
+    mov ah, 0eh      
+    int 0x10       
+    ret 
+
+message: db 'first bootloader msg', 0
+
 times 510- ($ - $$) db 0
 dw 0xAA55
